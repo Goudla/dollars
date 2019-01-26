632aa9cb38f0d0800d5e0cf067ad09458367daaa
Douglas Bamber <douglasbamber@gmail.com>
2019-01-27 09:41:05 +1300

    REFACTOR exports to be simpler

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
 ;(function(name, definition) {
-  /** Detect free variable `global` from Node.js. */
-  var freeGlobal = typeof global == 'object' && global && global.Object === Object && global;
-
-  /** Detect free variable `self`. */
-  var freeSelf = typeof self == 'object' && self && self.Object === Object && self;
-
-  /** Used as a reference to the global object. */
-  var root = freeGlobal || freeSelf || Function('return this')();
-
-  /** Detect free variable `exports`. */
-  var freeExports = typeof exports == 'object' && exports && !exports.nodeType && exports;
-
-  /** Detect free variable `module`. */
-  var freeModule = freeExports && typeof module == 'object' && module && !module.nodeType && module;
-
-  // Some AMD build optimizers, like r.js, check for condition patterns like:
-  if (typeof define == 'function' && typeof define.amd == 'object' && define.amd) {
-    define( name, [], function() {
-      return definition;
-    });
-  }
-  else if (freeModule) {
-    // Export for Node.js.
-    (freeModule.exports = definition())[name] = definition();
-    // Export for CommonJS support.
-    freeExports[name] = definition();
-  }
-  else {
-    // Export to the global object.
-    root[name] = definition();
+  if (typeof module != 'undefined') {
+    module.exports = definition();
+  } else if (typeof define == 'function' && typeof define.amd == 'object') {
+    define(name, [], definition);
+  } else {
+    this[name] = definition();
   }
 }('toDollars', function() {
   /**
